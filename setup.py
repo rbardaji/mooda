97da14d91717fd1c73c3c1683667ba5dcfba0c3e
Raul Bardaji <rbardaji@gmail.com>
2017-10-16 16:02:33 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='oceanobs',
+  packages=['oceanobs'],
+  version='1.0.0',
+  description='Package to analyze ocean observatories data.',
+  author='Raul Bardaji',
+  author_email='rbardaji@gmail.com',
+  url='https://github.com/rbardaji/oceanobs', # use the URL to the github repo
+  download_url='https://github.com/rbardaji/oceanobs/archive/1.0.tar.gz',
+  keywords=['observatory', 'ocean', 'sea', 'GUI', 'OBSEA', 'EMODnet', 'EMSOdev'],
+  classifiers=['Development Status :: 3 - Alpha', 'Topic :: Software Development :: Build Tools','License :: OSI Approved :: MIT License',],
+)
