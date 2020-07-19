bad5f892f6467d808a3791941b6302f728a540cc
Raul Bardaji <rbardaji@gmail.com>
2020-07-20 01:01:46 +0200

    update version

--- mooda/__init__.py
+++ mooda/__init__.py
@@ -3,4 +3,4 @@ from .waterframe import WaterFrame
 from .input import read_nc_emodnet, read_nc, read_nc_imos, read_pkl, read_nc_moist
 from .util import concat, iplot_location, iplot_timeseries, md5, es_create_indexes
 
-__version__ = '1.4.4'
+__version__ = '1.6.0'
