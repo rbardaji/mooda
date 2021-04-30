f9a7b56539c1668cb60ea4ba26093055a9dc7ff9
Raul Bardaji <rbardaji@gmail.com>
2021-04-30 15:35:11 +0200

    change version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 
 
 NAME = 'mooda'
-VERSION = '1.12.2'
+VERSION = '1.12.3'
 DESCRIPTION = 'Module for Ocean Observatory Data Analysis'
 LONG_DESCRIPTION = ("""
 MOODA - Module for Ocean Observatory Data Analysis
