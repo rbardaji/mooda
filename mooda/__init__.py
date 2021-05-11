8483e352fa3c134351c07294dc98f116bc1a12eb
Favio Medrano <hmedrano@macbook.pro>
2021-05-11 02:47:19 -0700

    from_erddap method using erddap-python library simplified functionality

--- mooda/__init__.py
+++ mooda/__init__.py
@@ -2,7 +2,7 @@
 
 from .waterframe import WaterFrame
 from .input import read_nc_emodnet, read_nc, read_nc_imos, read_pkl, \
-    read_nc_moist, from_emso, read_df
+    read_nc_moist, from_emso, read_df, from_erddap
 from .util import concat, iplot_location, iplot_timeseries, md5, \
     es_create_indexes, EMSO, widget_qc, widget_emso, widget_emso_qc, \
     widget_save, iplot_line
--- mooda/input/from_erddap.py
+++ mooda/input/from_erddap.py
@@ -1,26 +1,48 @@
-from erddapy import ERDDAP
+from typing import OrderedDict
+from erddapClient import ERDDAP_Tabledap
+from ..waterframe import WaterFrame
+from collections import OrderedDict
 
-# WORK IN PROGRESS
-
-def from_erddap(server, dataset_id, variables=None, constraints=None):
+def from_erddap(server, dataset_id, variables=None, constraints=None, rcsvkwargs={}, auth=None):
     """
-    Get a WaterFrame form an ERDDAP server.
+    Get a WaterFrame from an ERDDAP server tabledap dataset. 
 
     Parameters
     ----------
-        server: str
-            Server name.
+        server : The ERDDAP server URL        
+        dataset_id : The dataset id to query
+        variables : List of variables to get from ERDDAP server, it can be comma separated string or a list.
+        constraints : Query constraints to appy to the ERDDAP query, this can be list or dictionary.
+        read_csv_kwargs : Dictionary with the parameters to pass to the read_csv function that converts the ERDDAP response to pandas DataFrame
+        auth : Tupple with username and password to authenticate to a protected ERDDAP server.
     """
 
-    protocol = 'tabledap'
-    response = 'nc'
+    remote = ERDDAP_Tabledap(server, dataset_id, auth=auth)
 
-    e = ERDDAP(server, protocol, response)
+    # Build the query
+    if variables is None:
+        variables = list(remote.variables.keys())
+    remote.setResultVariables(variables) 
+    if constraints:
+        remote.addConstraints(constraints)
+    
+    # pandas read_csv parameters for the erddap csv request.
+    _rcsvkwargs = { **{ 'header' : 0, 
+                        'names' : variables } , **rcsvkwargs }
+    if 'time' in variables:
+        _rcsvkwargs['parse_dates'] = True
+        _rcsvkwargs['index_col'] = 'time'
 
-    e.dataset_id = dataset_id
-    e.variables = variables
-    e.constraints = constraints
+    # actual erddap data request
+    _df = remote.getDataFrame(**_rcsvkwargs)
 
-    ds = e.to_xarray(decode_times=False)
+    # waterframe params      
+    _vocabulary = OrderedDict([ (key,val) for key, val in remote.variables.items() if key in variables ])
+    wf = WaterFrame(df=_df,
+                    metadata=remote.info,
+                    vocabulary=_vocabulary)
 
-    return ds
+    # TODO variable names should be standarized? Ej. time -> TIME , temperature -> TEMP , etc? 
+    # TODO Should this method include arguments to request ERDDAP server side operations ? Ej. orderBy, orderByClosest, orderByMean, etc?
+
+    return wf
