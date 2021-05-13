d0b263abcae487cc69c36ad75fb997ae5c0b28d2
Favio Medrano <hmedrano@macbook.pro>
2021-05-13 00:31:09 -0700

    rename/create time,depth coordinate variables

--- mooda/input/from_erddap.py
+++ mooda/input/from_erddap.py
@@ -1,7 +1,6 @@
-from typing import OrderedDict
 from erddapClient import ERDDAP_Tabledap
-from ..waterframe import WaterFrame
 from collections import OrderedDict
+from ..waterframe import WaterFrame
 
 def from_erddap(server, dataset_id, variables=None, constraints=None, rcsvkwargs={}, auth=None):
     """
@@ -19,30 +18,51 @@ def from_erddap(server, dataset_id, variables=None, constraints=None, rcsvkwargs
 
     remote = ERDDAP_Tabledap(server, dataset_id, auth=auth)
 
-    # Build the query
+    # Build the ERDDAP query
     if variables is None:
         variables = list(remote.variables.keys())
     remote.setResultVariables(variables) 
     if constraints:
         remote.addConstraints(constraints)
     
-    # pandas read_csv parameters for the erddap csv request.
+    # parameters for the pandas read_csv method, which its used in the getDataFrame method
     _rcsvkwargs = { **{ 'header' : 0, 
                         'names' : variables } , **rcsvkwargs }
     if 'time' in variables:
-        _rcsvkwargs['parse_dates'] = True
-        _rcsvkwargs['index_col'] = 'time'
+        _rcsvkwargs['parse_dates'] = ['time']
 
     # actual erddap data request
     _df = remote.getDataFrame(**_rcsvkwargs)
-
-    # waterframe params      
+    # vocabulary subset from erddap
     _vocabulary = OrderedDict([ (key,val) for key, val in remote.variables.items() if key in variables ])
-    wf = WaterFrame(df=_df,
-                    metadata=remote.info,
-                    vocabulary=_vocabulary)
+
+    # Handle index columns names
+    for posible_depth_name in ['depth', 'altitude']:  # Special variable names in ERDDAP
+        if posible_depth_name in variables:
+            _df.rename(columns={posible_depth_name: 'DEPTH'}, inplace=True)
+            break
+    if not 'DEPTH' in _df.keys():
+        _df['DEPTH'] = 0
+
+    # time is a special variable in ERDDAP, it's always called time
+    _df.rename(columns={'time': 'TIME'}, inplace=True) 
+
+    # Add QC columns
+    keys = _df.keys()
+    for key in keys:
+        if key.endswith('_QC'):
+            continue
+        if f'{key}_QC' in keys:
+            continue
+        else:
+            _df[f'{key}_QC'] = 0
+
+    # Set index
+    _df.set_index(['DEPTH', 'TIME'], drop=True, inplace=True)
 
     # TODO variable names should be standarized? Ej. time -> TIME , temperature -> TEMP , etc? 
     # TODO Should this method include arguments to request ERDDAP server side operations ? Ej. orderBy, orderByClosest, orderByMean, etc?
 
-    return wf
+    return WaterFrame(df=_df,
+                      metadata=remote.info,
+                      vocabulary=_vocabulary)
